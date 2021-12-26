c7a948e56a33e69485498e082c011095e2cf9493
Ruelf <testingstuffuser@gmail.com>
2021-12-26 18:39:20 +0100

    Update /sarcasm script

--- sarcasm/index.js
+++ sarcasm/index.js
@@ -2,16 +2,9 @@ const textarea = document.querySelector("textarea");
 const div = document.querySelector("div");
 
 oninput = () => {
-    div.innerText = textarea.value
-        .split("\n")
-        .map(line => line
-            .split(" ")
-            .map(word => word
-                .split("")
-                .map((x, i) => i % 2 ? x.toUpperCase() : x.toLowerCase())
-                .join("")
-            )
-            .join(" ")
-        )
-        .join("\n");
+    div.innerText = textarea.value.replace(/\w+/g, (word => word
+        .split("")
+        .map((x, i) => i % 2 ? x.toUpperCase() : x.toLowerCase())
+        .join(""))
+    );
 };

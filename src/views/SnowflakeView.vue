0f74b1db0f87ab89bb98f6f94121292b65c9f74c
Ruelf <77465215+ruelf@users.noreply.github.com>
2025-01-22 23:39:32 +0100

    aa

---
+++ src/views/SnowflakeView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const now = ref<Date>();
+
+function update(): void {
+    now.value = new Date();
+}
+
+setInterval(update, 10);
+</script>
+
+<template>
+    <template v-if="now">
+        <h1>
+            <code>{{ BigInt(Math.floor(now.getTime() - 1420070400000)) << 22n }}</code>
+        </h1>
+        <p>{{ now.toDateString() }}</p>
+        <p>{{ now.toTimeString() }}</p>
+    </template>
+</template>

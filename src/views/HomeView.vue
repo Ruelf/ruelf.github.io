65d08b1b71f0e20b9bd5112de8d847586c61e025
Ruelf <77465215+ruelf@users.noreply.github.com>
2025-01-23 22:41:31 +0100

    raaaah

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+import router from '@/router';
+import { useRouter } from 'vue-router';
+
+console.log(2);
+function bruh() {
+    router.push({ name: 'snowflake' });
+}
+</script>
 <template>
-    Gaming
+    <div class="text-black">
+        Gaming
+    </div>
+
+    <button type="button" class="text-red-500" @click="bruh">aa</button>
 </template>
--- src/views/SnowflakeView.vue
+++ src/views/SnowflakeView.vue
@@ -1,21 +1,75 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import Card from '@/components/Card.vue';
+import { makeJolpicaRequest } from '@/helpers/helpers';
+import type { Driver, DriverTable, MRData } from '@/types/jolpica';
+import { onMounted, ref } from 'vue';
 
-const now = ref<Date>();
+const drivers = ref<Driver[]>();
+const data = ref<MRData<DriverTable>>();
 
-function update(): void {
-    now.value = new Date();
+onMounted(() => {
+    refreshDrivers({ limit: 10 });
+});
+
+async function refreshDrivers(params?: { limit?: number; offset?: number; }): Promise<void> {
+    const response = await makeJolpicaRequest<DriverTable>('/drivers', { params });
+    data.value = response.MRData;
+    drivers.value = response.MRData.DriverTable.Drivers;
 }
 
-setInterval(update, 10);
+function previousPage(): void {
+    if (!data.value) {
+        return;
+    }
+
+    refreshDrivers({ limit: +data.value.limit, offset: +data.value.offset - +data.value.limit });
+}
+
+function nextPage(): void {
+    if (!data.value) {
+        return;
+    }
+
+    refreshDrivers({ limit: +data.value.limit, offset: +data.value.offset + +data.value.limit });
+}
 </script>
 
 <template>
-    <template v-if="now">
-        <h1>
-            <code>{{ BigInt(Math.floor(now.getTime() - 1420070400000)) << 22n }}</code>
-        </h1>
-        <p>{{ now.toDateString() }}</p>
-        <p>{{ now.toTimeString() }}</p>
-    </template>
+    <Card class="p-4">
+        <table>
+            <thead>
+                <tr>
+                    <th>givenName</th>
+                    <th>familyName</th>
+                    <th>driverId</th>
+                    <th>dateOfBirth</th>
+                    <th>nationality</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="driver of drivers" :key="driver.driverId">
+                    <td>{{ driver.givenName }}</td>
+                    <td>{{ driver.familyName }}</td>
+                    <td>{{ driver.driverId }}</td>
+                    <td>{{ driver.dateOfBirth }}</td>
+                    <td>{{ driver.nationality }}</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <template v-if="data">
+            <div class="mt-4">
+                Page {{ +data.offset / +data.limit + 1 }} of {{ Math.ceil(+data.total / +data.limit) }}
+            </div>
+            <div class="mt-4">
+                Showing {{ data.offset }} to {{ +data.offset + +data.limit }} of {{ data.total }}
+            </div>
+        </template>
+
+
+        <div class="flex gap-4">
+            <button class="border border-red-500" @click="previousPage">Previous page</button>
+            <button class="border border-red-500" @click="nextPage">Next page</button>
+        </div>
+    </Card>
 </template>

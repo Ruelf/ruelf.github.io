52227e2651115d111660695e89a3ed73adc5e6e6
Ruelf <77465215+ruelf@users.noreply.github.com>
2025-01-30 20:35:28 +0100

    Install fontawesome

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(fas, far);
+
 const app = createApp(App);
 
 app.use(router);

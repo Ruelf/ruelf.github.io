ded94a85e29c861c3c404cfaa5f98ff13a1023a3
Ruelf <77465215+ruelf@users.noreply.github.com>
2025-02-06 20:53:20 +0100

    a

--- src/modules/jolpica/Result.ts
+++ src/modules/jolpica/Result.ts
@@ -1,5 +1,6 @@
 import { Constructor, type ConstructorApiData } from './Constructor';
 import { Driver, type DriverApiData } from './Driver';
+import type { Race } from './Race';
 import { Time, type TimeApiData } from './Time';
 
 export interface ResultApiData {
@@ -27,8 +28,9 @@ export class Result {
     laps: number;
     status: string;
     time?: Time;
+    Race: Race;
 
-    public constructor(data: ResultApiData) {
+    public constructor(data: ResultApiData, race: Race) {
         this.number = +data.number;
         this.position = +data.position;
         this.positionText = data.positionText;
@@ -38,6 +40,7 @@ export class Result {
         this.grid = +data.grid;
         this.laps = +data.laps;
         this.status = data.status;
+        this.Race = race;
 
         if (data.Time) {
             this.time = new Time(data.Time);

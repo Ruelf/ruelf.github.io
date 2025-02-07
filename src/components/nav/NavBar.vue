a4deddfaa806a05ed56c5656381a7ba43c9fed50
Ruelf <77465215+ruelf@users.noreply.github.com>
2025-02-07 23:33:52 +0100

    fix navbar

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import router from '@/router';
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue';
 
 interface NavItem {
@@ -46,10 +47,14 @@ const navItems: NavItem[] = [
                     </MenuButton>
 
                     <MenuItems class="absolute left-0 mt-2 min-w-32 overflow-hidden rounded-md dark:bg-gray-700">
-                        <MenuItem v-for="child of item.children" :key="child.title">
-                            <RouterLink :to="{ name: child.route }" class="block px-4 py-2 dark:hover:bg-gray-600">
-                                {{ child.title }}
-                            </RouterLink>
+                        <MenuItem
+                            v-for="child of item.children"
+                            :key="child.title"
+                            as="div"
+                            @click="router.push({ name: child.route })"
+                            class="block cursor-pointer px-4 py-2 dark:hover:bg-gray-600"
+                        >
+                            {{ child.title }}
                         </MenuItem>
                     </MenuItems>
                 </Menu>

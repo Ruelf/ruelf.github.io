5c34a25a55b32c85f5bfee47abc487a216327bbb
Ruelf <77465215+ruelf@users.noreply.github.com>
2025-02-09 21:36:58 +0100

    raah

--- src/views/f1/seasons/SeasonHome.vue
+++ src/views/f1/seasons/SeasonHome.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
 import Card from '@/components/Card.vue';
-import Table from '@/components/table/Table.vue';
-import Td from '@/components/table/Td.vue';
-import Th from '@/components/table/Th.vue';
-import Tr from '@/components/table/Tr.vue';
+import { Table, Td, Th, Tr } from '@/components/table';
 import { Jolpica, Race } from '@/modules/jolpica';
 import { onMounted, ref, watch } from 'vue';
 

5be8c497576c28cb736373322388c14e1f359d0e
Ruelf <77465215+ruelf@users.noreply.github.com>
2025-02-10 23:58:57 +0100

    fix z index

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex items-center">
         <div class="text-3xl">Yoo whats</div>
-        <div class="ms-8 inline rotate-12 scale-150 text-7xl text-green-500">up!</div>
+        <div class="-z-10 ms-8 inline rotate-12 scale-150 text-7xl text-green-500">up!</div>
     </div>
 
     <div class="mt-8 text-gray-400">Welcome to this epic site. I put random stuff here.</div>

d7cc5f534577ae6e39d53b62afed4cdc3d4fee94
Ruelf <77465215+ruelf@users.noreply.github.com>
2025-02-10 19:37:04 +0100

    bla

--- scripts/fix_404.py
+++ scripts/fix_404.py
@@ -1,5 +1,6 @@
+"""blablabla"""
 with (
-    open('./dist/index.html') as f_index,
-    open('./dist/404.html', 'w') as f_404,
+    open('./dist/index.html', encoding='UTF-8') as f_index,
+    open('./dist/404.html', 'w', encoding='UTF-8') as f_404,
 ):
     f_404.write(f_index.read())
